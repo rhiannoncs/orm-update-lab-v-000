0a8ac81f9901973df59560f7acb841677001e107
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-02-24 15:43:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,13 +3,13 @@ require_relative "../config/environment.rb"
 class Student
   attr_accessor :name, :grade
   attr_reader :id
-  
+
   def initialize(id = nil, name, grade)
     @id = id
     @name = name
     @grade = grade
   end
-  
+
   def self.create_table
     sql = <<-SQL
       CREATE TABLE students (
@@ -20,15 +20,15 @@ class Student
 
     DB[:conn].execute(sql)
   end
-  
+
   def self.drop_table
     sql = <<-SQL
       DROP TABLE students
     SQL
-    
+
     DB[:conn].execute(sql)
   end
-  
+
   def save
     if self.id
       self.update
@@ -37,41 +37,41 @@ class Student
         INSERT INTO students(name, grade)
         VALUES(?, ?)
       SQL
-    
+
       DB[:conn].execute(sql, self.name, self.grade)
-    
+
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
-  
+
   def self.create(name, grade)
     student = new(name, grade)
     student.save
     student
   end
-  
+
   def self.new_from_db(row)
     student = new(row[0], row[1], row[2])
   end
-  
+
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT * from students WHERE name = ?
     SQL
-    
+
     row = DB[:conn].execute(sql, name)
-    
+
     student = new_from_db(row)
     binding.pry
     student
   end
-  
+
   def update
     sql = <<-SQL
       UPDATE students SET name = ?, grade = ?
       WHERE id = ?
     SQL
-    
+
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 end
